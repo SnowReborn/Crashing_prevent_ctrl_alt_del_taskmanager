fceff4e8eb300df30fd44ed54a79a49fcc344d39
SnowReborn <36327107+SnowReborn@users.noreply.github.com>
2020-02-20 00:29:50 -0800

    Update Form1.vb

--- Form1.vb
+++ Form1.vb
@@ -3,7 +3,7 @@
     Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
         Me.Hide()
         Form2.Show()
-        MsgBox("Lagging!!! ", MsgBoxStyle.Information, "fuck")
+        MsgBox("Lagging!!! ", MsgBoxStyle.Information, "comptuer dieded")
         SendKeys.Send("{enter}")
         SendKeys.Send("123zxcvbny")
 
